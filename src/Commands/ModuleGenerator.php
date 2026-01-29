0443a745cc24a4033428e730bcc143b8914a9cc5
Bertuğ Fahri ÖZER <bertugfahriozer@gmail.com>
2026-01-29 18:13:36 +0300

    Enhances module generator with list view template
    Improves the module generator command by adding a dedicated view template for list pages.
    
    This template includes features such as DataTables integration, responsive design, and server-side processing, enhancing the user experience for list views in generated modules.

--- src/Commands/ModuleGenerator.php
+++ src/Commands/ModuleGenerator.php
@@ -52,9 +52,9 @@ class ModuleGenerator extends BaseCommand
         $this->createFile($modulePath . '/Language/en/' . $moduleName . '.php', $this->getLanguageTemplate($moduleName, 'en'));
         $this->createFile($modulePath . '/Language/tr/' . $moduleName . '.php', $this->getLanguageTemplate($moduleName, 'tr'));
 
-        $this->createFile($modulePath . '/Views/create.php', $this->getViewTemplate('<a href="'.route_to(lcfirst($moduleName)).'" class="btn btn-outline-info">'.lang('Backend.backToList').'</a>'));
-        $this->createFile($modulePath . '/Views/list.php', $this->getViewTemplate('<a href="'.route_to(lcfirst($moduleName).'Create').'" class="btn btn-outline-success">'.lang('Backend.add').'</a>'));
-        $this->createFile($modulePath . '/Views/update.php', $this->getViewTemplate('<a href="'.route_to(lcfirst($moduleName)).'" class="btn btn-outline-info">'.lang('Backend.backToList').'</a>'));
+        $this->createFile($modulePath . '/Views/create.php', $this->getViewTemplate('<a href="' . route_to(lcfirst($moduleName)) . '" class="btn btn-outline-info">' . lang('Backend.backToList') . '</a>'));
+        $this->createFile($modulePath . '/Views/list.php', $this->getViewTemplateList('<a href="' . route_to(lcfirst($moduleName) . 'Create') . '" class="btn btn-outline-success">' . lang('Backend.add') . '</a>'), $moduleName);
+        $this->createFile($modulePath . '/Views/update.php', $this->getViewTemplate('<a href="' . route_to(lcfirst($moduleName)) . '" class="btn btn-outline-info">' . lang('Backend.backToList') . '</a>'));
     }
 
     protected function createFile($path, $content)
@@ -66,7 +66,7 @@ class ModuleGenerator extends BaseCommand
 
     protected function getConfigTemplate($moduleName)
     {
-        $l_moduleName=lcfirst($moduleName);
+        $l_moduleName = lcfirst($moduleName);
         return <<<EOD
 <?php
 namespace Modules\\{$moduleName}\\Config;
@@ -102,7 +102,7 @@ EOD;
 
     protected function getControllerTemplate($moduleName)
     {
-        $l_moduleName=\lcfirst($moduleName);
+        $l_moduleName = \lcfirst($moduleName);
         return <<<EOD
 <?php
 namespace Modules\\{$moduleName}\\Controllers;
@@ -241,6 +241,130 @@ EOD;
 <?= \$this->endSection() ?>
 
 <?= \$this->section('javascript') ?>
+<?= \$this->endSection() ?>
+EOD;
+    }
+
+    protected function getViewTemplateList($button, $moduleName)
+    {
+        $l_moduleName = lcfirst($moduleName);
+        return <<<EOD
+<?= \$this->extend('Modules\\Backend\\Views\\base') ?>
+
+<?= \$this->section('title') ?>
+<?= lang(\$title->pagename) ?>
+<?= \$this->endSection() ?>
+
+<?= \$this->section('head') ?>
+<?= link_tag('be-assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>
+<?= link_tag('be-assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>
+<?= link_tag('be-assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>
+<?= \$this->endSection() ?>
+
+<?= \$this->section('content') ?>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1><?= lang(\$title->pagename) ?></h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    {$button}
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <!-- Default box -->
+    <div class="card card-outline card-shl">
+        <div class="card-header">
+            <h3 class="card-title font-weight-bold"><?= lang(\$title->pagename) ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th><?=lang('Backend.title')?></th>
+                            <th><?=lang('Backend.transactions')?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</section>
+<!-- /.content -->
+<?= \$this->endSection() ?>
+
+<?= \$this->section('javascript') ?>
+<?= script_tag('be-assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') ?>
+<?= script_tag('be-assets/plugins/jszip/jszip.min.js') ?>
+<?= script_tag('be-assets/plugins/pdfmake/pdfmake.min.js') ?>
+<?= script_tag('be-assets/plugins/pdfmake/vfs_fonts.js') ?>
+<?= script_tag('be-assets/plugins/datatables-buttons/js/buttons.html5.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-buttons/js/buttons.print.min.js') ?>
+<?= script_tag('be-assets/plugins/datatables-buttons/js/buttons.colVis.min.js') ?>
+<script>
+    let isApprove = true;
+    var table = $("#example1").DataTable({
+        responsive: true,
+        lengthChange: false,
+        autoWidth: false,
+        buttons: ["pageLength", {
+            text: "Refresh",
+            className: "btn btn-teal",
+            action: function(e, dt, node, config) {
+                dt.ajax.reload();
+            }
+        }],
+        processing: true,
+        pageLength: 10,
+        serverSide: true,
+        ordering: false,
+        lengthMenu: [10, 25, 50, {
+            label: 'All',
+            value: -1
+        }],
+        ajax: {
+            url: '<?= route_to('{$l_moduleName}') ?>',
+            type: 'POST',
+            data: {
+                isApproved: isApprove
+            }
+        },
+        columns: [{
+                data: 'title'
+            },
+            {
+                data: 'actions'
+            }
+        ],
+        initComplete: function() {
+            table.buttons().container()
+                .appendTo($('.col-md-6:eq(0)', table.table().container()));
+        }
+    });
+</script>
 <?= \$this->endSection() ?>
 EOD;
     }
